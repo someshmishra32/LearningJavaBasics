5e6e533ee6f4bf618482d2b8562d495c053b4ed4
someshmishra32 <someshmishra32@gmail.com>
2025-08-10 01:34:28 +0530

    Print 9 table using printf & println

---
+++ src/Task/Printf9table.java
@@ -0,0 +1,18 @@
+package Task;
+
+public class Printf9table {
+        public static void main(String[] args) {
+
+            System.out.printf("%d x %d = %d", 9, 1, 9).println();
+            System.out.printf("%d x %d = %d", 9, 2, 18).println();
+            System.out.printf("%d x %d = %d", 9, 3, 27).println();
+            System.out.printf("%d x %d = %d", 9, 4, 36).println();
+            System.out.printf("%d x %d = %d", 9, 5, 45).println();
+            System.out.printf("%d x %d = %d", 9, 6, 54).println();
+            System.out.printf("%d x %d = %d", 9, 7, 63).println();
+            System.out.printf("%d x %d = %d", 9, 8, 72).println();
+            System.out.printf("%d x %d = %d", 9, 9, 81).println();
+            System.out.printf("%d x %d = %d", 9, 10, 90).println();
+
+    }
+}
